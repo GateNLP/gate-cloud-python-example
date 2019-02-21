be2fb715339e432f27e47a08a7a1a10593450434
David Jones <drj@pobox.com>
2019-02-21 09:16:23 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,20 @@
+Refer to documentation here: https://cloud.gate.ac.uk/info/help/online-api.html
+
+and api browser here:
+https://cloud.gate.ac.uk/shopfront/displayItem/annie-named-entity-recognizer?fromApi=1
+
+requests make this really easy
+Need to set content-type
+
+put API key in .netrc https://ec.haxx.se/usingcurl-netrc.html
+
+machine cloud-api.gate.ac.uk
+login 71rs93h36m0c
+password 9u8ki81lstfc2z8qjlae
+
+
+# or twitie
+
+# Accept
+# Expect: 100?
+
---
+++ annie.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+# Using the GATE Cloud on-line API:
+# An example using ANNIE.
+
+# REFERENCES
+#
+# GATE Cloud on-line API
+#   https://cloud.gate.ac.uk/info/help/online-api.html
+#
+# ANNIE, English Named Entity Recognizer
+#   https://cloud.gate.ac.uk/shopfront/displayItem/annie-named-entity-recognizer
+
+# Python standard library https://docs.python.org/3.6/library/index.html
+import json
+import sys
+import urllib
+
+# pip install requests
+# https://pypi.org/project/requests/
+import requests
+
+# The base URL for all GATE Cloud API services
+prefix = "https://cloud-api.gate.ac.uk/"
+# The service point for ANNIE
+service = "process-document/annie-named-entity-recognizer"
+url = urllib.parse.urljoin(prefix, service)
+
+# Sample text
+text = "David Jones joined The University of Sheffield earlier this year."
+headers = {'Content-Type': 'text/plain'}
+
+# Make the API request and raise error if status >= 4xx
+response = requests.post(url, data=text, headers=headers)
+response.raise_for_status()
+
+# The requests library has a method for returning parsed JSON
+gate_json = response.json()
+
+# Pretty print the response, mostly for debugging
+print(json.dumps(gate_json, indent=2))
+
+# Find each annotation and print its type and the text it is annotating
+response_text = gate_json["text"]
+for annotation_type, annotations in gate_json['entities'].items():
+    for annotation in annotations:
+        i, j = annotation["indices"]
+        print(annotation_type, ":", response_text[i:j])
+
+
+# or twitie
+
+# HTTP Basic authentication
+# Content-Type
+# Accept
+# Expect: 100?
